8bde48451999b55e31097986b6be8ea1ab849fe5
Rsironi <ruansironi@hotmail.com>
2024-07-06 01:34:42 -0300

    first commit

---
+++ src/main/java/br/com/rsironn/relatorios_arquitetura_hexagonal/RelatoriosArquiteturaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsironn.relatorios_arquitetura_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatoriosArquiteturaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatoriosArquiteturaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsironn/relatorios_arquitetura_hexagonal/RelatoriosArquiteturaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsironn.relatorios_arquitetura_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatoriosArquiteturaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
